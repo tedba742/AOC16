0e695364a14a446e235773097e33eb8c47e950f4
tedba742 <157366463+tedba742@users.noreply.github.com>
2024-05-08 09:06:15 +0200

    forgot to upload day 6 pt 2

---
+++ day6/day6part2.cpp
@@ -0,0 +1,38 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <map>
+#include <limits>
+
+int main() {
+    std::fstream file{"input.txt"};
+    if (!file.is_open()) {
+        std::cerr << "Failed to open input.txt" << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    std::map<int, std::map<char, int>> charCount;
+
+    while (getline(file, line)) {
+        for (size_t i = 0; i < line.size(); ++i) {
+            char c = line[i];
+            charCount[i][c]++;
+        }
+    }
+
+    file.close();
+    std::string result{""};
+    for (const auto& idx_map : charCount) {
+        int maxCount = std::numeric_limits<int>::max();
+        char maxChar = 0;
+        for (const auto& pair : idx_map.second) {
+            if (pair.second < maxCount) {
+                maxCount = pair.second;
+                maxChar = pair.first;
+            }
+        }
+        result += maxChar;
+    }
+    std::cout << "Column " << result << std::endl;
+}
