169f1e34e67679562e943aabc19620274dedde11
tedba742 <157366463+tedba742@users.noreply.github.com>
2024-05-13 14:28:46 +0200

    Update day5part2.py

--- day5/day5part2.py
+++ day5/day5part2.py
@@ -2,26 +2,24 @@ from itertools import count
 from hashlib import md5
 
 door_id = 'uqwqemis'
-password = ['_'] * 8  # Use underscores as placeholders for unfilled positions
-found_positions = set()  # To track filled positions
+password = ['_'] * 8
+found_positions = set()
 
 for x in count():
     test = door_id + str(x)
     hash_result = md5(test.encode('utf-8')).hexdigest()
     if hash_result[:5] == '00000':
-        pos = hash_result[5]  # The potential position from the hash
+        pos = hash_result[5]
         
-        if pos.isdigit() and 0 <= int(pos) < 8:  # Check if it's a valid position
+        if pos.isdigit() and 0 <= int(pos) < 8:
             pos = int(pos)
-            if pos not in found_positions:  # Check if the position is already filled
-                char = hash_result[6]  # The character to place in the position
+            if pos not in found_positions:
+                char = hash_result[6]
                 password[pos] = char
                 found_positions.add(pos)
                 
-                # Print the current state of the password as a simple animation
                 print(''.join(password))
                 
-                # Break the loop if all positions are filled
                 if len(found_positions) == 8:
                     break
 
